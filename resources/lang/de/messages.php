a215ed08283b1937e1e6ba1bc7dd9f03863fb128
Josias Montag <josias@montag.info>
2017-08-06 13:00:07 +0200

    Add German localization

---
+++ resources/lang/de/messages.php
@@ -0,0 +1,25 @@
+
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Email Verification Language Lines
+    |--------------------------------------------------------------------------
+    */
+    'done' => 'Ihr Account wurde erfolgreich aktiviert!',
+    'sent' => 'Wir haben Ihnen eine Email mit Ihrem Aktivierungslink gesendet!',
+    'token' => 'Dieser Aktivierungslink ist ungültig!',
+    'user' => 'Der zugehörige Account zu dieser Email Adresse wurde nicht gefunden!',
+    'resend' => [
+        'title' => 'Email Verifikation',
+        'warning' => 'Wir haben Ihnen einen Aktivierungslink an <strong>:email</strong> gesendet. Bitte nutzen Sie diesen Link um Ihren Account zu aktivieren. Wenn Sie die Email nicht finden können, übeprüfen Sie bitte auch Ihren Junk/Spam Ordner!',
+        'instructions' => 'Falls Sie keine Email erhalten haben oder Sie sich bei Ihrer Email Adresse vertippt haben, können wir ihnen die Email erneut senden.',
+        'email' => 'Email',
+        'submit' => 'Aktivierungslink senden'
+    ],
+    'email' => [
+        'welcome' => 'Vielen Dank für Ihre Registierung!',
+        'instructions' => 'Um die Registrierung abzuschließen und Ihren Account zu aktivieren, nutzen Sie bitte den folgenden Link:',
+        'action' => 'Registierung abschließen',
+    ],
+];
